1f048d6b0e170494e16673bbb58bc7e0d5441e28
PorterNilsson <92951219+PorterNilsson@users.noreply.github.com>
2024-08-25 17:26:33 -0600

    Added road initialization and option to change hex schema

--- settlers/ascii_display.py
+++ settlers/ascii_display.py
@@ -12,20 +12,8 @@ class BoardDisplay:
         "WHITE": ""
         }
     INTERSECTION_TYPES = ("*", "s", "C")
-    HEX_ARRANGEMENT = (
-        (0,),
-        (-1, 1),
-        (-2, 0, 2),
-        (-1, 1),
-        (-2, 0, 2),
-        (-1, 1),
-        (-2, 0, 2),
-        (-1, 1),
-        (0,)
-    )
 
-
-    def __init__(self, size):
+    def __init__(self, size, schema=tuple(range(19))):
         # Size maps to the diagonal length of any individual hex.
         self.diagonal_len = BoardDisplay._validate_size(size)
         # Horizontal length in characters is longer as each monospaced character is smaller in width than height.
@@ -37,88 +25,80 @@ class BoardDisplay:
         self.top_height = 0
         # Number of newlines composing the middle portion of the board.
         self.middle_height = 0
-        # Updates "self.board", "self.top_height", and "self.middle_height"
         self._initialize_border()
 
-        # Array indices for the upper-left intersection of the first hex
+        # Board characteristics to determine row, col locations of all hexes and their intersections (offsets) relative to board size.
+        self.hex_schema = BoardDisplay._validate_schema(schema)
         self.hex_origin = self._get_hex_origin()
+        self.hex_locations = self._get_hex_locations()
+        print(self.hex_locations)
         self.hex_offsets = self._get_hex_offsets()
         self._initialize_intersections()
+        self._initialize_roads()
     
 
-    def set_intersection(self, hex, offset, color, intersection):
+    def set_intersection(self, hex, offset, color, intersection_type):
         if color not in BoardDisplay.COLORS.keys():
             print("Not a valid color. Must be 'RED', 'ORANGE', 'YELLOW', 'GREEN', 'BLUE', 'PURPLE', or 'WHITE'.")
             return
-        if intersection not in BoardDisplay.INTERSECTION_TYPES:
+        if intersection_type not in BoardDisplay.INTERSECTION_TYPES:
             print("Not a valid building type. Must be '*' for default, 's' for 'settlement', or 'C' for 'City'.")
             return
         
         row, col = self._get_location(hex, offset)
-        # print(f"Hex: {hex}, Offset: {offset}, Row: {row}, Col: {col}")
         if color == "WHITE":
-            self.board[row][col] = intersection
+            self.board[row][col] = intersection_type
         else:
-            self.board[row][col] = f"\x1b[38;2;{BoardDisplay.COLORS.get(color)}m{intersection}\x1b[0m"
+            self.board[row][col] = f"\x1b[38;2;{BoardDisplay.COLORS.get(color)}m{intersection_type}\x1b[0m"
 
 
-    # def set_road(self, color, hex, offset):
-    #     if color not in BoardDisplay.COLORS.keys():
-    #         print("Not a valid color. Must be 'RED', 'ORANGE', 'YELLOW', 'GREEN', 'BLUE', 'PURPLE', or 'WHITE'.")
-    #         return
+    def set_road(self, hex, offset, color):
+        next_offset = offset + 1
+        if next_offset == 6:
+            next_offset = 0
         
-    #     length = len(BoardDisplay.RELATIVE_HEX_LOCATIONS) - 1
-    #     if ((hex > length) 
-    #             or hex < (-1 * length) 
-    #             or not isinstance(hex, int)):
-    #         print(f"Not a valid intersection. Must be an integer in range [-{length - 1}, {length}], inclusive.")
-    #         return
-        
-    #     length = len(BoardDisplay.RELATIVE_INTERSECTION_LOCATIONS) - 1
-    #     if (offset > length
-    #             or offset < (-1 * length) 
-    #             or not isinstance(offset, int)):
-    #         print(f"Not a valid offset. Must be an integer in range [-{length - 1}, {length}] inclusive.")
-    #         return
-        
-    #     hex = BoardDisplay.RELATIVE_HEX_LOCATIONS[hex]
-    #     row = hex[0].split(',')
-    #     col = hex[1].split(',')
-    #     row = self._intersection_relative_location_calc(*row)
-    #     col = self._intersection_relative_location_calc(*col)
+        start = list(self._get_location(hex, offset))
+        end = list(self._get_location(hex, next_offset))
 
-    #     num = offset
-        
-    #     offset = BoardDisplay.RELATIVE_INTERSECTION_LOCATIONS[offset]
-    #     row_offset = offset[0].split(',')
-    #     col_offset = offset[1].split(',')
-    #     row += self._intersection_relative_location_calc(*row_offset)
-    #     col += self._intersection_relative_location_calc(*col_offset)
+        char = ""
+        row_increment = 0
+        col_increment = 0
 
-    #     d = self.diagonal_len
-    #     h = self.horizontal_len
-    #     direction_vectors = [
-    #         [0, h], [d, d], [d, -d], [0, -h], [-d, -d], [-d, d]
-    #     ]
-    #     road_types = ("-", "\\", "/", "-", "\\", "/")
+        row_diff = start[0] - end[0]
+        col_diff = start[1] - end[1]
+        # 6 conditions
+        if row_diff == 0 and col_diff < 0:
+            char = "-"
+            col_increment = 1
+        elif row_diff < 0 and col_diff < 0:
+            char = "\\"
+            row_increment = 1
+            col_increment = 1
+        elif row_diff < 0 and col_diff > 0:
+            char = "/"
+            row_increment = 1
+            col_increment = -1
+        elif row_diff == 0 and col_diff > 0:
+            char = "-"
+            col_increment = -1
+        elif row_diff > 0 and col_diff > 0:
+            char = "\\"
+            row_increment = -1
+            col_increment= -1
+        else:
+            char = "/"
+            row_increment = -1
+            col_increment = 1
 
-    #     v = direction_vectors[num]
-    #     while v != [0, 0]:
-    #         if color == "WHITE":
-    #             self.board[self.hex_origin[0] + row + v[0]][self.hex_origin[1] + col + v[1]] = road_types[num]
-    #         else:
-    #             self.board[self.hex_origin[0] + row + v[0]][self.hex_origin[1] + col + v[1]] = f"\x1b[38;2;{BoardDisplay.COLORS.get(color)}m{road_types[num]}\x1b[0m"
-        
-    #         if v[0] > 0:
-    #             v[0] -= 1
-    #         if v[1] > 0:
-    #             v[1] -= 1
-    #         if v[0] < 0:
-    #             v[0] += 1
-    #         if v[1] < 0:
-    #             v[1] += 1
-
-    #     return
+        start[0] += row_increment
+        start[1] += col_increment
+        while start != end:
+            if color == "WHITE":
+                self.board[start[0]][start[1]] = char
+            else:
+                self.board[start[0]][start[1]] = f"\x1b[38;2;{BoardDisplay.COLORS.get(color)}m{char}\x1b[0m"
+            start[0] += row_increment
+            start[1] += col_increment
 
 
     @staticmethod
@@ -166,49 +146,55 @@ class BoardDisplay:
 
         for i in range(middle_height):
             self.board.insert(i + self.top_height, list("|" + ((board_width - 2) * " ") + "|\n"))
-
-        # # Set Hex Origin (Must be done before set_intersection  is called)
-        # self.hex_origin = self._get_hex_origin()
-
-        # # Initialize Intersections and Roads
-        # for location in range(len(BoardDisplay.RELATIVE_HEX_LOCATIONS)):
-        #     for intersection in range(len(BoardDisplay.RELATIVE_INTERSECTION_LOCATIONS)):
-        #         self.set_intersection("WHITE", "*", location, intersection)
-        #         self.set_road("WHITE", location, intersection)
-
-        # # Initialize Ports
-        # for port in BoardDisplay.RELATIVE_PORT_LOCATIONS_AND_ROTATIONS:
-
-        #     hex = BoardDisplay.RELATIVE_HEX_LOCATIONS[port[0]]
-        #     row = hex[0].split(',')
-        #     col = hex[1].split(',')
-        #     row = self._intersection_relative_location_calc(*row)
-        #     col = self._intersection_relative_location_calc(*col)
-
-        #     offset = BoardDisplay.RELATIVE_INTERSECTION_LOCATIONS[port[1]]
-        #     row_offset = offset[0].split(',')
-        #     col_offset = offset[1].split(',')
-        #     row_one = self._intersection_relative_location_calc(*row_offset) + row
-        #     col_one = self._intersection_relative_location_calc(*col_offset) + col
-
-        #     offset = BoardDisplay.RELATIVE_INTERSECTION_LOCATIONS[port[1] + 1]
-        #     row_offset = offset[0].split(',')
-        #     col_offset = offset[1].split(',')
-        #     row_two = self._intersection_relative_location_calc(*row_offset) + row
-        #     col_two = self._intersection_relative_location_calc(*col_offset) + col
-            
-        #     # Filtering for beginning on a straightedge
-        #     row, col = self._draw_port_from_offset(row_one, col_one, port[2])
-
-        
-        # return self.board
     
+    @staticmethod
+    def _validate_schema(schema):
+        default_schema = range(19)
+        if len(schema) != 19:
+            print("Invalid schema length. Must be length 19 to account for each hex")
+            return default_schema
+        elif set(schema) != set(default_schema):
+            print("Invalid schema. Must contain all numbers 0-18 inclusive.")
+            return default_schema
+        elif type(schema) != tuple:
+            print("Invalid schema type. Must be a Tuple.")
+            return default_schema
+        else:
+            return schema
+
 
     def _get_hex_origin(self):
         # Finds indices of the top-middle hex's top-left intersection.
         row = self.top_height - (2 * self.diagonal_len) - 2
         col = (self.horizontal_len * 3) + (self.diagonal_len * 2) + 5
         return (row, col)
+
+
+    def _get_hex_locations(self):
+        hex_arrangement = (
+            (0,),
+            (-1, 1),
+            (-2, 0, 2),
+            (-1, 1),
+            (-2, 0, 2),
+            (-1, 1),
+            (-2, 0, 2),
+            (-1, 1),
+            (0,)
+        )
+
+        default_schema_locations = []
+        for i in range(len(hex_arrangement)):
+            for j in range(len(hex_arrangement[i])):
+                row = self.hex_origin[0] + (i * (self.diagonal_len + 1))
+                col = self.hex_origin[1] + (hex_arrangement[i][j] * (self.diagonal_len + self.horizontal_len + 2))
+                default_schema_locations.append((row, col))
+
+        new_schema_locations = []
+        for i in range(len(default_schema_locations)):
+            new_schema_locations.insert(self.hex_schema[i], default_schema_locations[self.hex_schema[i]])
+
+        return tuple(new_schema_locations)
 
 
     def _get_hex_offsets(self):
@@ -230,18 +216,16 @@ class BoardDisplay:
                 self.set_intersection(i, j, "WHITE", "*")
 
 
-    def _get_location(self, hex, offset):
-        row = 0
-        col = 0
-        for i in range(len(BoardDisplay.HEX_ARRANGEMENT)):
-            for j in range(len(BoardDisplay.HEX_ARRANGEMENT[i])):
-                if hex == 0:
-                    row = self.hex_origin[0] + (i * (self.diagonal_len + 1))
-                    col = self.hex_origin[1] + (BoardDisplay.HEX_ARRANGEMENT[i][j] * (self.diagonal_len + self.horizontal_len + 2))
-                hex -= 1
+    def _initialize_roads(self):
+        for i in range(19):
+            for j in range(6):
+                self.set_road(i, j, "WHITE")
+    
 
-        row += self.hex_offsets[offset][0]
-        col += self.hex_offsets[offset][1]
+    def _get_location(self, hex, offset):
+
+        row = self.hex_locations[hex][0] + self.hex_offsets[offset][0]
+        col = self.hex_locations[hex][1] + self.hex_offsets[offset][1]
 
         return row, col
 
